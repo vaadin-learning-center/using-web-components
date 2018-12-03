fe2ee2004943cb19257b4e86a9867406d4d4568c
Marcus Hellberg <marcus@vaadin.com>
2018-12-03 13:30:46 -0800

    starting point

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = ({ mode }) => {
   return {
@@ -7,14 +6,7 @@ module.exports = ({ mode }) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './src/index.html'
-      }),
-      new CopyWebpackPlugin([
-        {
-          context: 'node_modules/@webcomponents/webcomponentsjs',
-          from: '**/*.js',
-          to: 'webcomponents'
-        }
-      ])
+      })
     ],
     devtool: mode === 'development' ? 'source-map' : 'none'
   };
